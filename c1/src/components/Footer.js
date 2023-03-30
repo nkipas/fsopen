6dab05d5e621fb5d97aa3bb32f90afe90935ec42
nkipas <teme@moron.local>
2023-03-30 12:33:38 +0300

    setm_3

---
+++ c1/src/components/Footer.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+    const fstyle = {
+        color: 'green',
+        fontStyle: 'italic',
+        fontSize: 16
+    }
+
+    return (
+        <div style={fstyle}>
+        <br />
+        <em>Note app, DoD</em>
+        </div>
+    )
+}
+
+export default Footer
